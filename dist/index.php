017ea3ba69fbb5b4e6fc1b269cbe7f48a3f19d2e
Bo Skakke <bo@skakke.dk>
2015-01-13 17:19:15 +0100

    tuesday

--- dist/index.php
+++ dist/index.php
@@ -1,24 +1,50 @@
 <? require_once("../../dummy/dummy.php") ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="da">
   <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Business.dk</title>
-    <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600,700' rel='stylesheet' type='text/css'>
-    <link href="css/bootstrap.css" rel="stylesheet">
-
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
+    <?php include("./inc/head.php"); ?> 
   </head>
   <body>
+  <!--[if lt IE 9]>
+      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+  <![endif]-->
+
+
+  <header class="site-header container">
+    <div class="row">
+    
+    <div class="col-sm-6">
+      <nav class="super-header">
+        <ul>
+          <li><a href="#">Berlingske.dk</a></li>
+          <li><a href="#">Politiko.dk</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="col-sm-6">
+      <nav class="super-header text-right">
+        <ul>
+          <li><a href="#" class="sosume">Privatlivspolitik</a></li>
+          <li><a href="#">Annoncør</a></li>
+          <li><a href="#">Kontakt</a></li>
+        </ul>
+      </nav>
+    </div>
+      <div class="col-sm-12">
+        <a href="/" class="logo">
+          Berlingske Business
+        </a>    
+      </div>
+    </div>
+    
+  </header>
   <div class="container">
     
     <div class="row">
       <section class="deck">
+
+      
       <div class="col-md-12">
           <h1 class="section-header">Seneste nyt</h1>
       </div>
@@ -57,8 +83,7 @@
   </div><!-- container -->
     
 
-
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
+  <?php include("./inc/footer.php"); ?>
+    
   </body>
 </html>
