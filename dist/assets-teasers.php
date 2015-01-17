25e5bfda57496507065cda73f81fecb6cccbc395
boskakke <bo@skakke.dk>
2015-01-18 00:59:21 +0100

    article

--- dist/assets-teasers.php
+++ dist/assets-teasers.php
@@ -13,7 +13,64 @@
 
   <?php // include("./inc/header.php"); ?>
 
+
+
   <div class="container">
+
+
+
+  <section class="deck">
+      <div class="row">
+        <div class="col-md-12">
+            <h1 class="section-header">Business Premium<a href="#" class="pull-right">Se alle</a></h1>
+        </div>
+
+     
+          <ul class="list-clean flex-4-2-1">
+            <? while (dumb_luck("4")): ?>
+              <li>
+                <article class="teaser teaser-ecommerce col-md-3 col-sm-4 col-xs-6">
+                  <figure class="teaser-img">
+                    <a href="article.php"><img src="<? dummy("image/!ecommerce-biz@400x,16:9") ?>" width="" height="" alt="" class="img-responsive" /></a>
+                  </figure>
+                  
+                  <h2 class="header"><a href="article.php"><? dummy("text@ecommerce") ?></a></h2>
+                  <div class="teaser-price"><a href="http://www.berlingske.dk/shop" target="_blank" class="btn btn-xxs btn-secondary">Køb</a> <span class="price"><? dummy("text@number") ?> kr.</span></div>
+                </article>
+                </li>
+            <? endwhile ?>
+          </ul>
+
+          
+        </div>
+      </section>
+
+      <section class="deck">
+        <div class="row">
+        <div class="col-md-12">
+            <h1 class="section-header">Gode tilbud i Berlingske Shop <a href="#" class="pull-right">Gå til shoppen</a></h1>
+
+        </div>
+
+        
+        
+          <ul class="list-clean flex-4-2-1">
+            <? while (dumb_luck("3")): ?>
+              <li>
+                <article class="teaser teaser-ecommerce col-md-4 col-sm-4 col-xs-6">
+                  <figure class="teaser-img">
+                    <a href="article.php"><img src="<? dummy("image/!ecommerce-biz@400x,16:9") ?>" width="" height="" alt="" class="img-responsive" /></a>
+                  </figure>
+                  
+                  <h2 class="header"><a href="article.php"><? dummy("text@ecommerce") ?></a></h2>
+                  <div class="teaser-price"><a href="http://www.berlingske.dk/shop" target="_blank" class="btn btn-xxs btn-secondary">Køb</a> <span class="price"><? dummy("text@number") ?> kr.</span></div>
+                </article>
+                </li>
+            <? endwhile ?>
+          </ul>
+
+      </section>
+
 
   <div class="row">
     
@@ -50,7 +107,7 @@
     </div>
 
     <div class="col-md-4">
-      <ul class="toplist toplist-large">
+      <ul class="toplist toplist-lg">
         <? while (dumb_luck("10")): ?>
           <li><a href="article.php"><? dummy("text@headline") ?></a></li>
         <? endwhile ?>
