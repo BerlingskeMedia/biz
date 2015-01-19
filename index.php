37a37cfd7be978cfbbf51ce0c7c76c4fbf45f953
Bo Skakke <bo@skakke.dk>
2015-01-19 16:34:24 +0100

    page

--- index.php
+++ index.php
@@ -23,7 +23,55 @@
       <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
   <![endif]-->
 
+<style type="text/css">
+body {
+  background: #fff;
+}
+  li:after {
+    color: rgba(0,0,0,.4);
+    display: inline-block;
+    padding-left: 10px;
+    font-style: italic;
+    font-size: 12px;
+  }
+  li a {
+    padding: 0 4px;
+    text-shadow: 1px 1px 0 rgba(255,255,255,.4);
+  }
+  /* Not started */
+  li.a a {
+    background: rgba(0,0,0,.05);
+    color: #333;  
+  }
+  li.a:after  {
+    content: 'Not started'; 
+  }
+  /* In pregress */
+  li.b a {
+    background: rgba(255,204,51,.3);
+    color: #663300;
+  }
+  li.b:after  {
+    content: 'In progress'; 
+  }
+  /* Done - needs approval */
+  li.c a {
+    background: rgba(204,102,0,.5);
+    color: #663300;
+  }
+  li.c:after  {
+    content: 'Needs approval'; 
+  }
 
+  /* Ready for development */
+  li.d a {
+    background: rgba(0,255,0,.2);
+    color: #336633;
+  }
+  li.d:after  {
+    content: 'Approved and ready'; 
+  }
+</style>
   
   <div class="container">
     
@@ -33,39 +81,41 @@
 
     <div class="row">
 
-    <section class="deck">
-      <div class="col-md-12 text-center">
+    
+      <div class="col-md-12 text-center module">
       <br><br>
       <a href="overview.php" class="logo">
           Berlingske Business
         </a>    
       </div>
-      <div class="col-dm-12">
+      <div class="col-md-6">
         
 
         <h3>Page templates</h3>
 
-        <h4>Generel page templates</h4>
+        <h4>Generel</h4>
 
         <ul class="overview">
-        <li><a href="/dist/index.php">Frontpage</a></li>
-        <li><a href="/dist/article.php">Article</a></li>
+        <li class="a"><a href="/dist/index.php">Frontpage</a></li>
+        <li class="b"><a href="/dist/article.php">Article</a></li>
 
-        <li><a href="/dist/staff.php">Staff</a></li>
+        <li class="c"><a href="/dist/staff.php">Staff</a></li>
         </ul>
 
         <h4>Business <span class="logo-premium"><span>Premium</span></span></h4>
         <ul>
-        <li><a href="/dist/frontpage-premium.php">Frontpage</a>
-        <li><a href="/dist/section-premium.php">Section</a>
-        <li><a href="/dist/article-premium.php">Article</a>
+        <li class="b"><a href="/dist/frontpage-premium.php">Frontpage</a>
+        <li class="b"><a href="/dist/section-premium.php">Section</a>
+        <li class="b"><a href="/dist/article-premium.php">Article</a>
           
-            <li><a href="/dist/page-text-premium-1.php">Help page 1</a></li>
-            <li><a href="/dist/page-text-premium-2.php">Help page 2</a></li>
-            <li><a href="/dist/page-text-premium-3.php">Help page 3</a></li>
-            <li><a href="/dist/page-text-premium-4.php">Help page 4</a></li>
+          <li class="b"><a href="/dist/page-text-premium-1.php">Help page 1</a></li>
+          <li class="b"><a href="/dist/page-text-premium-2.php">Help page 2</a></li>
+          <li class="b"><a href="/dist/page-text-premium-3.php">Help page 3</a></li>
+          <li class="b"><a href="/dist/page-text-premium-4.php">Help page 4</a></li>
+          <li class="a"><a href="#">Premium buy article</a></li>
+          <li class="a"><a href="#">My premium articles</a></li>
           
-        </li>
+        
         </ul>
         <h3>Toolbox</h3>
 
@@ -76,9 +126,19 @@
 
         
       </div>
-    </section>
+    
+<aside class="col-md-6">
+<h3>People</h3>
+    <p><a href="mailto:mih@berlingskemdia.dk">Mikael Hjort</a> - 25 45 63 49</p>
+    <p><a href="mailto:xjewa@berlingskemdia.dk">Jens Wartenberg</a> - 29 86 10 10</p>
+    <p><a href="mailto:tvf@berlingskemdia.dk">Troels Frederiksen</a> - 27 22 20 80</p>
+    <p><a href="mailto:bsk@berlingskemdia.dk">Bo Skakke</a> - 20 61 67 68</p>
+    </aside>
+
 
     </div>
+
+    
 
   
 
