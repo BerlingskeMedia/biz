ff6df1e665dd7b7524c091bead325c346ea90163
Bo Skakke <bo@skakke.dk>
2015-01-19 13:11:48 +0100

    premium

--- dist/article-premium.php
+++ dist/article-premium.php
@@ -34,7 +34,11 @@
 
       
       <article class="col-md-12">
+        
+        <?php include('inc/article-header-premium.php'); ?>
+
         <header class="article-head">
+
 
           <a href="section.php" title="TITLE" class="article-tag"><span><? dummy("text@biz-cat") ?></span></a>
           <h1 class="heading"><? dummy("text@headline") ?></h1>
@@ -72,6 +76,7 @@
         </div>
 
         <div class="row">
+        
           <div class="col-md-8 col-md-push-4">
           
             <div class="article-content">
@@ -285,39 +290,12 @@
           <aside class="col-md-4 col-md-pull-8">
           <div class="article-sidebar">
 
-          <section class="module business-inventory business-inventory-jobindex">
-              <h1 class="module-header">Ledige job fra Jobindex A/S</h1>
-              <ul class="list-jobs">
-              <? while (dumb_luck("3")): ?>
-              <li>
-              <a href="#">
-                <strong><? dummy("text@job") ?></strong>
-                <span class="job-company"><? dummy("text@job-company") ?></span>
-              </a>
-              </li>
-              <? endwhile ?>
-              </ul>
-              <footer class="inventory-footer">
-                <a href="#">Se flere jobs på Jobindex.dk <i class="fa fa-angle-right"></i></a>
-              </footer>
-          </section>
+          
+          
 
-            <div class="module-sm banner">
-              <? dummy("ad@300x250") ?>
-            </div>
-              <section class="module-sm list list-border list-xs list-image-left">
-                
-                  <? while (dumb_luck("4")): ?>
-                    <article class="teaser">
-                      <figure><a href="article.php"><img src="<? dummy("image@60x60,") ?>" width="" height="" alt="" /></a></figure>
-                      <div class="teaser-body">
-                        <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
-                        <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a>  <time> <? dummy("text@time-ago-short") ?></time></footer>
-                      </div>
-                    </article>
-                  <? endwhile ?>
-                
-              </section>
+          
+            
+              
             </div><!-- article-sidebar -->
           </aside><!-- col -->
 
---
+++ dist/inc/article-header-premium.php
@@ -0,0 +1,4 @@
+<header class="header-premium">
+  <h1 class="site-title logo-premium"><a href="section-premium.php"><span>Premium</span></a></h1>
+  <nav class="small"><a href="#">Aktivér abonnement</a> <a href="#">FAQ</a><a href="#">Tilmeld nyhedsbrev</a><a href="#" data-toggle="modal" data-target="#modal-login">Log ind</a></nav>
+</header>
