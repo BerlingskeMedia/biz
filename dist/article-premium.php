0c6b192a9bfdf7c83f70ddc4a1782a21cb346014
Bo Skakke <bo@skakke.dk>
2015-01-19 12:12:22 +0100

    premium

--- dist/article-premium.php
+++ dist/article-premium.php
@@ -8,7 +8,7 @@
     <title>Business.dk</title>
     <?php include("./inc/head.php"); ?> 
   </head>
-  <body>
+  <body class="article-category-premium">
   <!--[if lt IE 9]>
       <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
   <![endif]-->
@@ -86,66 +86,97 @@
               </div>
 
               
-              
-              
-              
-              <h2 class="article-summary"><? dummy("text@long-teaser") ?></h2>
-              <? if (dumb_luck("25%")): ?>
-                <!-- *** Asset - factbox theme-->
-                <?php include("inc/asset-article-factbox-theme.php"); ?>
-              <? endif ?>
+  
 
 
-              
 
-              
-              <ul><li>Albatros Travel</li><li>AdventureDK</li><li>Atlantis</li><li>Akademisk Rejsebureau</li><li>Limpopo Travel</li></ul>
-              
 
-              <? while (dumb_luck("3")): ?><p><? dummy("text@paragraph") ?></p><? endwhile ?>
-              
-              
-              <p><strong>Dette får du også med i analysen:</strong></p>
+<h2 class="article-summary"><? dummy("text@long-teaser") ?></h2>
 
-              <ul>
+
+
+
+<p>Rejsebranchen blev - ligesom de fleste andre brancher - også berørt af finanskrisen, men ikke i lige så høj grad som man kunne frygte. Selskabernes primære udgifter består af personaleomkostninge,hvilket man relativt hurtigt kunne nedbringe som følge af faldende indtægter.</p>
+<p>De 60 medvirkende selskaber har samlet set løftet deres bruttofortjeneste med 42 mio. kroner, svarende til 4 pct. i perioden fra 2011 til 2013. Driftsresultatet og årets resultat har i samme periode oplevet endnu større vækstrater, med henholdsvis 23 pct. og 14 pct. i løbet af de seneste tre år. Selskabernes egenkapital er samlet set øget med 134 mio.kroner i løbet af analyseperioden, og det selvom, aktionærerne i de seneste to år er blevet forkælet med 331,5 mio. kroner i aktieudbytte. At branchen er i en sund økonomisk forfatning, understreges yderligere af at kun seks ud af de 60 medvirkende selskaber havde røde tal på bundlinjen i 2013, mens tallet var 12 i 2011.</p>
+<p>Selvom der overordnet tegner sig et billede af en meget lukrativ branche, er der flere forhold som kunderne og politikkerne skal være opmærksom på. Et stort problem for rejsebureauerne er det<br>tilsyn som dansketablerede rejsebureauer siden 2004 har været underlagt i form af rejsegarantifondsloven. Fonden fører nu et meget nidkært tilsyn med rejsebureauerne, og ifølge Lars Thykier, hæmmer det væksten for de danske rejsebureauer i en sådan grad, at det er skadeligt for de dansk-etablerede rejsebureauer.</p>
+<p><strong>Dette får du også med i analysen:</strong></p>
+<ul>
 <li>Analyse af egenkapitalforrentning Soliditetsgrad og afkastningsgrad</li>
 <li>Interview med nøgleperson fra branchen</li>
 <li>Konkurrence fra udlandet</li>
 <li>Politiske tiltag</li>
 <li>Forventninger til fremtiden</li>
 </ul>
+<p>Analysen koster 990,- i styk-salg, men ved at købe et abonnement til Business Premium får du - udover analysen - også adgang til alt andet indhold i Premium-universet.</p>
+<p><strong>Køb analysen nedenfor og download den direkte fra siden.</strong></p>
 
-<div class="article-fact  article-fact-premium fact-2-col">
-<h4 class="aside-header">Det får du med analysen af 60 rejsebureauer</h4>
-<p><strong>Du får en 14 siders analyse skrevet af en analysemedarbejder i Berlingske Research i en PDF-fil skrevet på baggrund af de senest tilgængelige 3 årsregnskab&nbsp; (2013, 2012 og 2011).</strong></p><p><strong>Du får også en Excel-fil, som indeholder:</strong></p><ul><li>Grafik-ark med bl.a. bruttofortjenester, soliditet, egenkapitaler på brancheniveau</li><li>Indtjeningsanalyse (afkastningsgrad, egenkapitalforretning, payout etc.)</li><li>Vækstanalyse (bruttofortjeneste, aktiver, egenkapital fra 2011 til 2013)</li><li>Virksomhedernes rangering efter afkastningsgrad</li><li>Regnskabstal for de tre senest tilgængelige regnskaber</li><li>Kontaktoplysninger (adresse, mail og telefon, samt navn på adm. direktør)</li></ul><p><strong>Vi har samlet følgende oplysninger om hver virksomhed:</strong></p><p>CVR, virksomhedstype (f.eks. A/S), nettoomsætning (sporadisk registreret), bruttofortjeneste, primært resultat, finansielle indtægter, ordinært resultat før finansielle udgifter, finansielle udgifter, resultat før skat og ekstraordinære poster, ekstraordinære indtægter/udgifter (meget sporadisk registreret), ordinært resultat før skat, skat, ordinært resultat efter skat, minoritetsandel, årets resultat, dividende, henlagt til egenkapital, aktiver, likvider, egenkapital ultimo, minoritetsinteresser (meget sporadisk registreret), hensættelser, langfristet gæld, kortfristet gæld, antal medarbejdere (sporadisk registreret), startdato.</p><p><strong>De 60 virksomheder</strong></p><ul><li>Albatros Travel</li><li>AdventureDK</li><li>Atlantis</li><li>Akademisk Rejsebureau</li><li>Limpopo Travel</li><li>Stjernegaard Rejser</li><li>Dagama Travel</li><li>Unitas Rejser</li><li>Australienrejser</li><li>Euro Travel</li><li>Best Travel</li><li>Blixen Tours</li><li>Bravo Tours</li><li>C &amp; C Travel</li><li>Check Point Travel</li><li>Club La Santa</li><li>Dahl Andersen</li><li>Dansk Bilferie</li><li>Viktors farmor</li><li>Detur</li><li>Discovery Rejsebureau</li><li>FDM Travel</li><li>Top Tours</li><li>Fonden 65-Ferie</li><li>Magic Tours</li><li>Gislev Rejser</li><li>Grace Tours</li><li>Grupperejsebureauet</li><li>Holstebro Rejsecenter</li><li>Horizons</li><li>Seatravel</li><li>Comet Rejser</li><li>Kilroy Group Travel</li><li>Kipling Travel</li><li>Rejseselskabet Jesper Hannibal &amp; Company</li><li>MyCruise</li><li>MyPlanet International</li><li>Nortlander</li><li>Nyhavn Rejser</li><li>DTF Travel</li><li>Peacock Travel</li><li>Primo Tours</li><li>Profil Rejser</li><li>Rejsespecialisten</li><li>Riis Rejser</li><li>Risskov Travel Partner</li><li>Horisont Rejser</li><li>Spies</li><li>Star Tour</li><li>Sun Tours</li><li>Svane Rejsebureau</li><li>Take Offer</li><li>Topas</li><li>USATours.dk</li><li>Vacansoleil Danmark</li><li>Ruby Rejser</li><li>Happydays</li><li>Århus Charter</li><li>Store Verden Safari</li><li>Live Travel</li></ul><p>Du downloader PDF-rapporten og Excel-arket og kan gemme filerne, så længe du ønsker.</p><p>Kilder: Analysen er udarbejdet på baggrund af virksomhedernes tre seneste tilgængelige regnskaber. I analysen frasorteres virksomheder der ikke offentliggør regnskaber.</p>
+<div class="article-fact article-fact-premium ">
+<h4 class="header">Det får du med analysen af 60 rejsebureauer</h4><p><strong>Du får en 14 siders analyse skrevet af en analysemedarbejder i Berlingske Research i en PDF-fil skrevet på baggrund af de senest tilgængelige 3 årsregnskab&nbsp; (2013, 2012 og 2011).</strong></p><p><strong>Du får også en Excel-fil, som indeholder:</strong></p><ul><li>Grafik-ark med bl.a. bruttofortjenester, soliditet, egenkapitaler på brancheniveau</li><li>Indtjeningsanalyse (afkastningsgrad, egenkapitalforretning, payout etc.)</li><li>Vækstanalyse (bruttofortjeneste, aktiver, egenkapital fra 2011 til 2013)</li><li>Virksomhedernes rangering efter afkastningsgrad</li><li>Regnskabstal for de tre senest tilgængelige regnskaber</li><li>Kontaktoplysninger (adresse, mail og telefon, samt navn på adm. direktør)</li></ul><p><strong>Vi har samlet følgende oplysninger om hver virksomhed:</strong></p><p>CVR, virksomhedstype (f.eks. A/S), nettoomsætning (sporadisk registreret), bruttofortjeneste, primært resultat, finansielle indtægter, ordinært resultat før finansielle udgifter, finansielle udgifter, resultat før skat og ekstraordinære poster, ekstraordinære indtægter/udgifter (meget sporadisk registreret), ordinært resultat før skat, skat, ordinært resultat efter skat, minoritetsandel, årets resultat, dividende, henlagt til egenkapital, aktiver, likvider, egenkapital ultimo, minoritetsinteresser (meget sporadisk registreret), hensættelser, langfristet gæld, kortfristet gæld, antal medarbejdere (sporadisk registreret), startdato.</p><p><strong>De 60 virksomheder</strong></p><ul><li>Albatros Travel</li><li>AdventureDK</li><li>Atlantis</li><li>Akademisk Rejsebureau</li><li>Limpopo Travel</li><li>Stjernegaard Rejser</li><li>Dagama Travel</li><li>Unitas Rejser</li><li>Australienrejser</li><li>Euro Travel</li><li>Best Travel</li><li>Blixen Tours</li><li>Bravo Tours</li><li>C &amp; C Travel</li><li>Check Point Travel</li><li>Club La Santa</li><li>Dahl Andersen</li><li>Dansk Bilferie</li><li>Viktors farmor</li><li>Detur</li><li>Discovery Rejsebureau</li><li>FDM Travel</li><li>Top Tours</li><li>Fonden 65-Ferie</li><li>Magic Tours</li><li>Gislev Rejser</li><li>Grace Tours</li><li>Grupperejsebureauet</li><li>Holstebro Rejsecenter</li><li>Horizons</li><li>Seatravel</li><li>Comet Rejser</li><li>Kilroy Group Travel</li><li>Kipling Travel</li><li>Rejseselskabet Jesper Hannibal &amp; Company</li><li>MyCruise</li><li>MyPlanet International</li><li>Nortlander</li><li>Nyhavn Rejser</li><li>DTF Travel</li><li>Peacock Travel</li><li>Primo Tours</li><li>Profil Rejser</li><li>Rejsespecialisten</li><li>Riis Rejser</li><li>Risskov Travel Partner</li><li>Horisont Rejser</li><li>Spies</li><li>Star Tour</li><li>Sun Tours</li><li>Svane Rejsebureau</li><li>Take Offer</li><li>Topas</li><li>USATours.dk</li><li>Vacansoleil Danmark</li><li>Ruby Rejser</li><li>Happydays</li><li>Århus Charter</li><li>Store Verden Safari</li><li>Live Travel</li></ul><p>Du downloader PDF-rapporten og Excel-arket og kan gemme filerne, så længe du ønsker.</p><p>Kilder: Analysen er udarbejdet på baggrund af virksomhedernes tre seneste tilgængelige regnskaber. I analysen frasorteres virksomheder der ikke offentliggør regnskaber.</p>
 </div>
 
+              
+              
+              <p><strong>Dette får du også med i analysen:</strong></p>
+
+              <ul>
+        <li>Analyse af egenkapitalforrentning Soliditetsgrad og afkastningsgrad</li>
+        <li>Interview med nøgleperson fra branchen</li>
+        <li>Konkurrence fra udlandet</li>
+        <li>Politiske tiltag</li>
+        <li>Forventninger til fremtiden</li>
+        </ul>
+
+       
+
 
               
               
 
+          <div class="article-premium-buy ">
+            
+            <h1>Køb adgang til denne artikel</h1>
 
-              
+          <div class="text-center"><p>Allerede PREMIUM-medlem? <a href="#" data-toggle="modal" data-target="#modal-login">Log ind her</a></p></div>
 
+          <div class="row">
+            <div class="col-sm-4">
+              <div class="premium-buy-option">
+                <span class="premium-buy-title">Styksalg</span>
+              <span class="premium-price">990 kr.</span>  
+              <footer>
+                <button class="btn btn-xs btn-primary">Køb adgang</button>  
+              </footer>
+              </div>
+            </div>
+            <div class="col-sm-4">
+            <div class="premium-buy-option">
+            <span class="premium-buy-title">Fri adgang 6 måneder</span>
+            <span class="premium-price">3.000 kr.</span>
+              <footer>
+                <button class="btn btn-xs btn-primary">Køb adgang</button>  
+              </footer>
+            </div>
               
+            </div>
+            <div class="col-sm-4">
+            <div class="premium-buy-option">
+            <span class="premium-buy-title">Fri adgang 1 år</span>
+            <span class="premium-price">5.000 kr.</span>
+              <footer>
+                <button class="btn btn-xs btn-primary">Køb adgang</button>  
+              </footer>
+            </div>
+              
+              
+            </div>
 
-              
+            <div class="col-sm-12">
+              <p class="module-top small text-center"><a href="#">Hvad er PREMIUM?</a> - <a href="#">Brug for hjælp?</a></p>
+            </div>
+          </div>
+          
 
-              
-
-              
-
-              
-              
-              
-              
-
-              
-
-              
-              
-
-              
+          </div>
               
 
               
@@ -169,56 +200,67 @@
               </div>
 
               <!-- READ ALSO -->
-              <aside class="article-related module-top-lg module">
+              
+                
+                <section class="module-top-lg module">
                 
               <h1 class="section-header">Read also</h1>
               
-
-              <ul class="list list-xs">
-              <? while (dumb_luck("2-5")): ?>
-              <li class="teaser">
-              <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
-              <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a> <time><? dummy("text@time-ago-short") ?></time></footer>
-              <? endwhile ?>  
+              <ul class="list list-xs ">
+              <? while (dumb_luck("5")): ?>
+                <li class="teaser">
+                  <div class="teaser-body">
+                  <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
+                  <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a> <time><? dummy("text@time-ago-short") ?></time></footer>
+                  </div>
+                  </li>
+                <? endwhile ?>
               </ul>
 
-              </aside><!-- read also -->
+              
+
+              </section><!-- read also -->
+              
 
 
               <div class="row">
                 
               <div class="col-md-6">
                 
-                 <aside class="article-related">
+                  <section>
                 
               <h1 class="section-header">Read also</h1>
               
-
-              <ul class="list list-xs">
+              <ul class="list list-xs ">
               <? while (dumb_luck("5")): ?>
-              <li class="teaser">
-              <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
-              <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a> <time><? dummy("text@time-ago-short") ?></time></footer>
-              <? endwhile ?>  
+                <li class="teaser">
+                  <div class="teaser-body">
+                  <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
+                  <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a> <time><? dummy("text@time-ago-short") ?></time></footer>
+                  </div>
+                  </li>
+                <? endwhile ?>
               </ul>
 
-              </aside><!-- read also -->
+              
+
+              </section><!-- read also -->
 
 
               </div><!-- col -->
               <div class="col-md-6">
+              
+              <section>
               <h1 class="section-header">Most read</h1>
-                  
-                  <ul class="toplist toplist-lg list-xs list-image-right ">
-                    <? while (dumb_luck("5")): ?>
-                      <li class="header">
-                      <? if (dumb_luck("66%")): ?><figure class="teaser-img"><img src="<? dummy("image@50x50,") ?>" width="" height="" alt="" /></figure><? endif ?>
-                      <div class="teaser-body">
-                        <a href="article.php"><? dummy("text@headline") ?></a>
-                      </div>
-                      </li>
-                    <? endwhile ?>
-                  </ul>
+                <ul class="list toplist toplist-lg list-xs list-image-right">
+                  <? while (dumb_luck("5")): ?>
+                    <li class="teaser">
+                    <? if (dumb_luck("66%")): ?><figure class="teaser-img"><a href="article.php"><img src="<? dummy("image@50x50,") ?>" width="" height="" alt="" /></a></figure><? endif ?>
+                    <div class="teaser-body"><h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2></div>
+                    </li>
+                  <? endwhile ?>
+                </ul>
+              </section>
 
               </div>
               </div>
@@ -291,7 +333,7 @@
 
         
         
-          <ul class="list-clean flex-4-2-1">
+          <ul class="list-clean flex-4-2-2">
             <? while (dumb_luck("8")): ?>
               <li>
                 <article class="teaser col-md-3 col-sm-3 col-xs-6">
--- dist/article.php
+++ dist/article.php
@@ -234,49 +234,52 @@
               </div>
 
               <!-- READ ALSO -->
-              <aside class="article-related module-top-lg module">
+               
+                <section class="module-top-lg module">
                 
               <h1 class="section-header">Read also</h1>
               
-
-              <ul class="list list-xs">
-              <? while (dumb_luck("2-5")): ?>
-              <li class="teaser">
-              <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
-              <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a> <time><? dummy("text@time-ago-short") ?></time></footer>
-              <? endwhile ?>  
+              <ul class="list list-xs ">
+              <? while (dumb_luck("5")): ?>
+                <li class="teaser">
+                  <div class="teaser-body">
+                  <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
+                  <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a> <time><? dummy("text@time-ago-short") ?></time></footer>
+                  </div>
+                  </li>
+                <? endwhile ?>
               </ul>
 
-              </aside><!-- read also -->
+              </section><!-- read also -->
+              
 
 
               <div class="row">
                 
               <div class="col-md-6">
                 
-                 <aside class="article-related">
+                 <section>
                 
               <h1 class="section-header">Read also</h1>
               
               <ul class="list list-xs ">
-        <? while (dumb_luck("5")): ?>
-          <li class="teaser">
-          
-          <div class="teaser-body"><h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2></div>
-          </li>
-        <? endwhile ?>
-      </ul>
+              <? while (dumb_luck("5")): ?>
+                <li class="teaser">
+                  <div class="teaser-body"><h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2></div>
+                  </li>
+                <? endwhile ?>
+              </ul>
 
               
 
-              </aside><!-- read also -->
+              </section><!-- read also -->
 
 
               </div><!-- col -->
               <div class="col-md-6">
               
                   
-                  <section>
+      <section>
     <h1 class="section-header">Most read</h1>
       <ul class="list toplist toplist-lg list-xs list-image-right">
         <? while (dumb_luck("5")): ?>
