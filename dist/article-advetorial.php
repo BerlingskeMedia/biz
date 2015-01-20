8b32b44dac4adb0e78e0e69294df05353eea78a1
Bo Skakke <bo@skakke.dk>
2015-01-20 12:58:32 +0100

    advetorail

--- dist/article-advetorial.php
+++ dist/article-advetorial.php
@@ -8,7 +8,7 @@
     <title>Business.dk</title>
     <?php include("./inc/head.php"); ?> 
   </head>
-  <body>
+  <body class="article-advetorial">
   <!--[if lt IE 9]>
       <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
   <![endif]-->
