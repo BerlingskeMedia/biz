163944476a409252ef89ba5d2185ba92b6c81407
Bo Skakke <bo@skakke.dk>
2015-01-30 13:54:00 +0100

    cs

--- dist/inc/article-content-stream.php
+++ dist/inc/article-content-stream.php
@@ -1,38 +1,216 @@
 
 
-<section class="module-sm list  list-xs list-image-left">
+<style type="text/css">
+  .article-sidebar section {
+    padding-bottom: 20px;
+  }
   
+</style>
+
+<section class="">
+    
+      <ul class="list list-sx">
     <? while (dumb_luck("4")): ?>
-      <article class="teaser">
-        <figure><a href="article.php"><img src="<? dummy("image@60x60,") ?>" width="" height="" alt="" /></a></figure>
-        <div class="teaser-body">
+      
+        <li class="teaser">
+            <div class="teaser-body">
           <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
           <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a>  <time> <? dummy("text@time-ago-short") ?></time></footer>
-        </div>
-      </article>
+        </div>    
+        </li>
     <? endwhile ?>
+    </ul>
   
 </section>
 
-
-<section class="business-inventory business-inventory-jobindex">
-<h1 class="module-header">Ledige job fra Jobindex A/S</h1>
-<ul class="list-jobs">
-<? while (dumb_luck("3")): ?>
-<li>
-<a href="#">
-  <strong><? dummy("text@job") ?></strong>
-  <span class="job-company"><? dummy("text@job-company") ?></span>
-</a>
-</li>
-<? endwhile ?>
-</ul>
-<footer class="inventory-footer">
-  <a href="#">Se flere jobs på Jobindex.dk <i class="fa fa-angle-right"></i></a>
-</footer>
-</section>
 
 <div class="module-sm banner">
 <? dummy("ad@300x250") ?>
 </div>
+
+
+<section class="">
+    
+      <ul class="list list-sx">
+    <? while (dumb_luck("4")): ?>
+      
+        <li class="teaser">
+            <div class="teaser-body">
+          <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
+          <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a>  <time> <? dummy("text@time-ago-short") ?></time></footer>
+        </div>    
+        </li>
+    <? endwhile ?>
+    </ul>
+  
+</section>
+
+<section class="">
+<div class="business-inventory business-inventory-jobindex">
+  
+
+  <h1 class="aside-header">Jobindex A/S</h1>
+  <ul class="list-jobs">
+  <? while (dumb_luck("3")): ?>
+  <li>
+  <a href="#">
+    <strong><? dummy("text@job") ?></strong>
+    <span class="job-company"><? dummy("text@job-company") ?></span>
+  </a>
+  </li>
+  <? endwhile ?>
+  </ul>
+  <footer class="inventory-footer">
+    <a href="#">Se flere jobs på Jobindex.dk <i class="fa fa-angle-right"></i></a>
+  </footer>
+  </div>
+</section>
+
+<section class="">
+    <ul class="list list-sx">
+    <? while (dumb_luck("4")): ?>
+        <li class="teaser">
+            <div class="teaser-body">
+          <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
+          <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a>  <time> <? dummy("text@time-ago-short") ?></time></footer>
+        </div>    
+        </li>
+    <? endwhile ?>
+    </ul>
+
+</section>
+
+
+<div class="module-sm banner">
+<? dummy("ad@300x250") ?>
+</div>
+
+
+<section class="">
+    
+      <ul class="list list-sx">
+    <? while (dumb_luck("4")): ?>
+      
+        <li class="teaser">
+            <div class="teaser-body">
+          <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
+          <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a>  <time> <? dummy("text@time-ago-short") ?></time></footer>
+        </div>    
+        </li>
+    <? endwhile ?>
+    </ul>
+  
+</section>
+
+
+<section >
+
+<h1 class="section-header"><span class="logo-premium">Premium</span> <a href="section.php" class="section-header-link">Se alle <i class="fa fa-caret-right"></i></h1>
+<div class="row">
+<? while (dumb_luck("2")): ?>
+<div class="col-xs-6">
+<article class="teaser teaser-no-logo">
+  <figure class="teaser-img"><a href="article-premium.php"><img src="<? dummy("image@200x,16:9") ?>" width="" height="" alt="" /></a></figure>
+  <h2 class="header"><? dummy("text@headline") ?></h2>
+  <p class="price"><? dummy("text@number") ?> kr <span class="logo-premium"><span>PREMIUM</span></span></p>
+</article>
+</div>
+<? endwhile ?>
+</div>
+</section>
+
+
+
+<section class="">
+    
+      <ul class="list list-sx">
+    <? while (dumb_luck("4")): ?>
+      
+        <li class="teaser">
+            <div class="teaser-body">
+          <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
+          <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a>  <time> <? dummy("text@time-ago-short") ?></time></footer>
+        </div>    
+        </li>
+    <? endwhile ?>
+    </ul>
+  
+</section>
+
+
+<section >
+  <h1 class="aside-header">BrandView <a href="section.php" class="section-header-link">Se alle <i class="fa fa-caret-right"></i></h1>
+  <div class="row">
+<? while (dumb_luck("2")): ?>
+<div class="col-xs-6">
+<article class="teaser">
+  <figure class="teaser-img"><a href="article-premium.php"><img src="<? dummy("image@200x,16:9") ?>" width="" height="" alt="" /></a></figure>
+  <span class="teaser-tagline">
+                    Sponseret af Columbus
+                  </span>
+  <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
+  
+</article>
+</div>
+<? endwhile ?>
+</div>
+
+</section>
+
+
+
+
+<section class="">
+    
+      <ul class="list list-sx">
+    <? while (dumb_luck("4")): ?>
+      
+        <li class="teaser">
+            <div class="teaser-body">
+          <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
+          <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a>  <time> <? dummy("text@time-ago-short") ?></time></footer>
+        </div>    
+        </li>
+    <? endwhile ?>
+    </ul>
+  
+</section>
+
+
+
+<section>
+  <h1 class="aside-header">Shopping <a href="section.php" class="section-header-link">Flere tilbud <i class="fa fa-caret-right"></i></h1>
+  <div class="row">
+<? while (dumb_luck("2")): ?>
+<div class="col-xs-6">
+<article class="teaser">
+  <figure class="teaser-img"><a href="article-premium.php"><img src="<? dummy("image/!ecommerce-biz@200x,16:9") ?>" width="" height="" alt="" /></a></figure>
+  <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
+  <div class="teaser-price"><a href="http://www.berlingske.dk/shop" target="_blank" class="btn btn-xxs btn-secondary">Køb</a> <span class="price"><? dummy("text@number") ?> kr.</span></div>
+
+</article>
+</div>
+<? endwhile ?>
+</div>
+</section>
+
+
+
+
+
+<section class="">
+    
+      <ul class="list list-sx">
+    <? while (dumb_luck("4")): ?>
+      
+        <li class="teaser">
+            <div class="teaser-body">
+          <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
+          <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a>  <time> <? dummy("text@time-ago-short") ?></time></footer>
+        </div>    
+        </li>
+    <? endwhile ?>
+    </ul>
+  
+</section>
 
