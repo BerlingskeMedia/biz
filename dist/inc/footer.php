60ddb8589d15cf82fc73b7aed3480ad372f127a3
Bo Skakke <bo@skakke.dk>
2015-02-18 12:43:04 +0100

    cookie

--- dist/inc/footer.php
+++ dist/inc/footer.php
@@ -98,24 +98,4 @@ Annoncesalg
 	</div>
 
 </footer>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-
-<!-- Responsive videos -->
-<script src="js/plugins/jquery.fitvids.js"></script>
-
-<!-- Image slider -->
-<script src="js/plugins/slick.min.js"></script>
-
-
-<script>
-  $('#myButton').on('click', function () {
-    var $btn = $(this).button('loading')
-    // business logic...
-    $btn.button('reset')
-  })
-</script>
-
-
-<script src="js/app.js?ver=<? dummy("text@number") ?>"></script>
+<?php include('/inc/scripts.php'); ?>
---
+++ dist/inc/scripts.php
@@ -0,0 +1,21 @@
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+
+<!-- Responsive videos -->
+<script src="js/plugins/jquery.fitvids.js"></script>
+
+<!-- Image slider -->
+<script src="js/plugins/slick.min.js"></script>
+
+
+<script>
+  $('#myButton').on('click', function () {
+    var $btn = $(this).button('loading')
+    // business logic...
+    $btn.button('reset')
+  })
+</script>
+
+
+<script src="js/app.js?ver=<? dummy("text@number") ?>"></script>
