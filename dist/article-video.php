9ba9e87c8bb3b0f1aecc0a1bdd166615ff9ee7b2
Bo Skakke <bo@skakke.dk>
2015-03-03 10:50:29 +0100

    video article

--- dist/article-video.php
+++ dist/article-video.php
@@ -52,13 +52,6 @@
 
             <?php include('inc/asset-article-byline.php'); ?>
 
-
-          
-         
-
-        
-          
-
           
 
         </header>
@@ -68,20 +61,12 @@
         </div>
 
         <div class="row">
-          <div class="col-md-8 col-md-push-4">
+          <div class="col-md-12 ">
           
             <div class="article-content">
-              
-                
-
             
 
               <div class="article-video"></div>
-
-            
-
-              
-
               
 
               <div class="article-social article-social-top">
@@ -92,17 +77,37 @@
               </div>
 
               
+                
               
-              
-              
-              <h2 class="article-summary"><? dummy("text@long-teaser") ?></h2>
-
-              
-
-
-
+                <h2 class="article-summary"><? dummy("text@long-teaser") ?></h2>
               <? if (dumb_luck("50%")): ?><p><? dummy("text@paragraph") ?></p><? endif ?>
+              
 
+                <div class="comments" id="article-comments">
+
+                <div id="disqus_thread"></div>
+                <script type="text/javascript">
+                    /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
+                    var disqus_shortname = 'biztesting'; // required: replace example with your forum shortname
+                    /* * * DON'T EDIT BELOW THIS LINE * * */
+                    (function() {
+                        var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
+                        dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
+                        (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
+                    })();
+                </script>
+                <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+    
+
+
+              </div><!-- comments -->
+
+              
+              
+              
+              
+              
+              
 
                
               
@@ -113,11 +118,7 @@
               <?php include('inc/asset-article-tags.php'); ?>
 
 
-              <div class="article-social text-center">
-                    <a href="#" class="icon-social-facebook"><i class="fa fa-facebook"></i></a>
-                    <a href="#" class="icon-social-twitter"><i class="fa fa-twitter"></i></a>
-                    <a href="#" class="icon-social-linkedin"><i class="fa fa-linkedin"></i></a>
-              </div>
+
 
 
 
@@ -125,36 +126,38 @@
 
 
               
-              <div class="comments" id="article-comments">
-                
-             
-
-                <div id="disqus_thread"></div>
-    <script type="text/javascript">
-        /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-        var disqus_shortname = 'biztesting'; // required: replace example with your forum shortname
-
-        /* * * DON'T EDIT BELOW THIS LINE * * */
-        (function() {
-            var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-            dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
-            (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-        })();
-    </script>
-    <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
-    
+              
 
 
-              </div>
 
               <section class="row module-sm module-top">
 
-              
               <div class="col-md-12">
                   <h1 class="section-header">
-                    <? dummy("text@biz-cat") ?>
+                    Latest Business TV
                   </h1>
               </div>  
+
+          <? while (dumb_luck("2")): ?>
+
+          <div class="col-md-6">
+          <article class="teaser teaser-video">
+          <figure class="teaser-img"><a href="article-video.php"><img src="<? dummy("image@720x,16:9") ?>" width="" height="" alt="" class="img-responsive" /></a></figure>
+          <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a>  <time> 3t.</time></footer>
+          <? dummy("text@biz-headertags") ?>
+          <h2 class="header"><a href="article-video.php">
+          <? dummy("text@headline") ?></a></h2>
+        </article>
+        </div>
+
+            <? endwhile ?>
+
+            
+            </section>
+
+            <section class="row module-sm">
+
+              
               
 
       <ul class="list-clean flex-4-2-2">
@@ -171,7 +174,13 @@
       <? endwhile ?>
     </ul>
     
+
+      <div class="col-md-12 text-center">
+                <button class="btn btn-sm btn-primary">Load more <i class="fa fa-caret-down"></i></button>
+            </div>
     </section>
+
+    <? while (dumb_luck("4")): ?>
 
      <section class="row  deck">
 
@@ -198,6 +207,11 @@
     </ul>
     
     </section>
+
+    <? endwhile ?>
+
+     
+
     
 
 
@@ -205,17 +219,8 @@
             </div><!-- article-content -->
           </div><!-- col -->
           
-          <!-- SIDEBAR  -->
-          <aside class="col-md-4 col-md-pull-8">
-          <div class="article-sidebar article-content-stream">
 
 
-          <?php  include('inc/article-content-stream.php'); ?>
-
-          
-          
-            </div><!-- article-sidebar -->
-          </aside><!-- col -->
 
 
         </div><!-- row -->
