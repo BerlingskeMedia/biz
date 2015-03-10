193d7c0306da5d83b6926cb03ee08ef04f0db46c
Bo Skakke <bo@skakke.dk>
2015-03-10 15:20:50 +0100

    header

--- dist/inc/header.php
+++ dist/inc/header.php
@@ -93,7 +93,7 @@
             </li>
           </ul>
           <div class="mobile-icons">
-          Følg:
+          <span>Følg</span>
             <a href="#"><i class="fa fa-facebook"></i></a>
             <a href="#"><i class="fa fa-twitter"></i></a>
             <a href="#"><i class="fa fa-linkedin"></i></a>
