e1511b7b0f9821bc2c07aee22f986ea4bc62115b
Bo Skakke <bo@skakke.dk>
2015-03-11 15:30:23 +0100

    header

--- dist/inc/header.php
+++ dist/inc/header.php
@@ -3,6 +3,11 @@
 <aside class="mobile-nav">
   <a href="#" class="mobile-login" data-toggle="modal" data-target="#modal-login"><i class="fa fa-user"></i> <small>Log ind</small></a>
   <a class="close-menu" href="#"><i class="fa fa-times"></i></a>
+  <div class="site-search">
+    <form action="./search-result.php" method="POST">
+    <input type="search" class="" placeholder="What are you searching for?"> 
+    </form>
+  </div>
 
   <ul class="mobile-main-nav">
     <li ><a href="section.php">Investor</a><a href="#" class="show-sub"><i class="fa fa-angle-down"></i></a>
@@ -67,7 +72,7 @@
           <li><a href="#" class="btn btn-primary ">Buy subscription</a></li>
          </ul>
 
-         <ul class="pull-right nav-tools">
+         <ul class="pull-right nav-tools hidden-xs">
           <li class="social-icons">Follow us: <a href="#"><i class="fa fa-facebook"></i></a> <a href="#"><i class="fa fa-twitter"></i></a> <a href="#"><i class="fa fa-linkedin"></i></a></li>  
           <li><a href="#" class="sosume">Privacy</a></li>
         </ul>
@@ -88,20 +93,12 @@
   <div class="row">
     <div class="header-bottom col-md-12">
         <a href="./" class="logo-business">Business</a>
-        <a href="#" class="btn-toggle-nav"><i class="fa fa-bars"></i></a>
-        <a href="#" class="btn-toggle-mobile-search toggle-global-search"><i class="fa fa-search"></i></a>
+        
+        <!-- <a href="#" class="btn-toggle-mobile-search toggle-global-search"><i class="fa fa-search"></i></a> -->
         <nav class="site-nav">
-
-
-
-
-        
-
-        
-
-          <ul class="main-nav">
-            <li class="menu-md"><a href="section.php">Investor</a><a href="#" class="show-sub"><i class="fa fa-angle-down"></i></a>
-            <ul class="sub-menu">
+          <ul class="main-nav desktop-nav">
+            <li ><a href="section.php">Investor</a>
+            <ul class="sub-menu desktop-nav">
               <li><a href="section.php">Se alle</a></li>
               <li><a href="section.php">Økonomi</a></li>
               <li><a href="section.php">Global</a></li>
@@ -113,25 +110,25 @@
               <li><a href="section.php">Markeder</a></li>
             </ul>
             </li>
-            <li class="menu-md"><a href="section.php">Virksomheder</a><a href="#" class="show-sub"><i class="fa fa-angle-down"></i></a>
+            <li ><a href="section.php">Virksomheder</a>
             <ul class="sub-menu">
               <li><a href="section.php">Se alle</a></li>
               <? while (dumb_luck("6-9")): ?><li><a href="section.php"><? dummy("text@item") ?></a></li><? endwhile ?>
             </ul>
             </li>
-            <li class="menu-md"><a href="section.php">Privatøkonomi</a><a href="#" class="show-sub"><i class="fa fa-angle-down"></i></a>
+            <li ><a href="section.php">Privatøkonomi</a>
             <ul class="sub-menu">
               <li><a href="section.php">Se alle</a></li>
               <? while (dumb_luck("6-9")): ?><li><a href="section.php"><? dummy("text@item") ?></a></li><? endwhile ?>
             </ul>
             </li>
-            <li class="menu-md <? if (dumb_luck("25%")): ?>active<? endif ?>"><a href="section.php">Karriere</a><a href="#" class="show-sub"><i class="fa fa-angle-down"></i></a>
+            <li class="<? if (dumb_luck("25%")): ?>active<? endif ?>"><a href="section.php">Karriere</a>
             <ul class="sub-menu">
               <li><a href="section.php">Se alle</a></li>
               <? while (dumb_luck("6-9")): ?><li><a href="section.php"><? dummy("text@item") ?></a></li><? endwhile ?>
             </ul>
             </li>
-            <li class="menu-md"><a href="section.php">TV</a><a href="#" class="show-sub"><i class="fa fa-angle-down"></i></a>
+            <li ><a href="section.php">TV</a><a href="#" class="show-sub"></a>
             <ul class="sub-menu">
               <li><a href="section.php">Se alle</a></li>
               <? while (dumb_luck("6-9")): ?><li><a href="section.php"><? dummy("text@item") ?></a></li><? endwhile ?>
@@ -140,10 +137,11 @@
           </ul>
           
 
-          <ul class="nav-tools main-nav">
-            <li class=" menu-latest"><a href="./"><i class="fa fa-clock-o"></i><ins><? dummy("text@number") ?></ins></a></li>
-            <li class="menu-md menu-search"><a href="./"><i class="fa fa-search"></i></a></li>
-            <li class="menu-login"><a href="#" data-toggle="modal" data-target="#modal-login"><i class="fa fa-user"></i> <small>Log ind</small></a></li>
+          <ul class="main-nav nav-tools">
+            <li class="menu-menu"><a href="#" class="btn-toggle-nav"><i class="fa fa-bars"></i></a></li>
+            <li class="menu-latest"><a href="./"><i class="fa fa-clock-o"></i><ins><? dummy("text@number") ?></ins></a></li>
+            <li class="menu-search hidden-xs"><a href="./"><i class="fa fa-search"></i></a></li>
+            <li class="menu-login hidden-xs"><a href="#" data-toggle="modal" data-target="#modal-login"><i class="fa fa-user"></i> <small>Log ind</small></a></li>
           </ul>
         
         </nav>
