0452280320939d10db0e0fc7b16dfa6227079af3
Bo Skakke <bo@skakke.dk>
2015-04-03 14:16:05 +0200

    wait

--- index.php
+++ index.php
@@ -177,6 +177,7 @@ li ul {
           <li class="d"><a href="./dist/gallery-overlay-finale.php">Image galleries - Symfony - final slide</a></li>
           <li class="d"><a href="./dist/assets-cookiewarning.php">Cookiewarning</a></li>
           <li class="d"><a href="./dist/section-archive.php">Article archive</a></li>
+          <li class="b"><a href="./dist/wait.php">Under construction</a></li>
 
         </ul>
 
