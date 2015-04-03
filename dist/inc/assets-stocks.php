fa056b2415e1e4e45594df9a58f1514363111afb
Bo Skakke <bo@skakke.dk>
2015-04-03 12:36:42 +0200

    stock info

--- dist/inc/assets-stocks.php
+++ dist/inc/assets-stocks.php
@@ -29,45 +29,23 @@
         </nav>
           
         </div>
-        <div class="col-md-12">
-        
-        <div class="stock-overview module-sm">
-          <ul class="stock-info">
-          <? while (dumb_luck("8")): ?>
-            <li class="<? if (dumb_luck("50%")): ?>stock-green<? else: ?>stock-red<? endif ?>">
-            <a href="#">
-              <b><? dummy("text@stocknames") ?></b>
-              -0,39%</a>
-            </li>
-          <? endwhile ?>
-          <li class="stock-search">
-              <form class="form-signup">
-            <div class="form-group form-oneline ">
-              <input type="text"  class="form-control input-sm" placeholder="Search stock">
-              <button class="btn btn-sm btn-primary btn-trans ">Søg <i class="fa fa-caret-right"></i></button>
-            </div>
-          </form>
-            </li>
-        </ul>
-
-        </div>
         
         
+          
+          <div class="col-md-4 col-sm-6">
+            <h2 class="micro-header">OMX C20 CAP</h2>
+            <div class="stock-graph">
+              <img src="http://businessdk.netdania.com/backend/GetImage.aspx?instr=OMXC20CAP.co&prv=ms_dlc&points=288&ts=5&current={CURR}&w=300&h=200&bgcolor=fadcd2&isFP=true&st=false&tp=1" class="img-responsive">
+              <p class="micro">Kursdata kan være forsinket 15 minutter</p>
+          </div>
+          </div>
+
+          <div class="col-md-8 col-sm-6">
 
 
-
-        
-
-        </div>
-
-      
-      <div class="col-md-4">
-      <h2 class="micro-header">OMX C20 CAP</h2>
-          <img src="http://businessdk.netdania.com/backend/GetImage.aspx?instr=OMXC20CAP.co&prv=ms_dlc&points=288&ts=5&current={CURR}&w=300&h=200&bgcolor=f5dfda&isFP=true&st=false&tp=1" class="img-responsive">
-          <p class="micro">Kursdata kan være forsinket 15 minutter</p>
-      </div>
-
-      <div class="col-md-4 col-sm-6">
+            <div class="row">
+              
+              <div class="col-md-6 col-sm-12">
       <h2 class="micro-header">Vindere</h2>
       
           <div class="stock-deck table-responsive">
@@ -97,7 +75,7 @@
 
 
       </div>
-      <div class="col-md-4 col-sm-6">
+      <div class="col-md-6 col-sm-12">
       <h2 class="micro-header">Tabere</h2>
            <div class="stock-deck table-responsive">
             
@@ -124,6 +102,16 @@
 
           </div>
       </div>
+
+
+            </div>
+          </div>
+      
+      
+      
+
+
+      
       
       <div class="col-md-12">
         <div class="section-sponsor-lg"><a href="http://www.nykredit.dk/#!/dit-liv/bolig/boligfinansiering/dagens-obligationskurser-udbetaling?ref=businessdk" target="_blank"><i class="fa fa-caret-right"></i>  Se de aktuelle obligationskurser</a> <a href="http://www.nykredit.dk/#!/dit-liv/bolig/boligfinansiering/dagens-obligationskurser-udbetaling?ref=businessdk" target="_blank"><img class="sponsor-logo" src="images/nykredit_logo_negativ.jpg"></a></div>
