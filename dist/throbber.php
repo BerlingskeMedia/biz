f34552dbfb1974d9c1b6cae364bbd736eed7450e
Bo Skakke <bo@skakke.dk>
2015-04-08 13:16:20 +0200

    added throbber example

---
+++ dist/throbber.php
@@ -0,0 +1,49 @@
+<? require_once("../../dummy/dummy.php") ?>
+
+<?php $imageSize = rand(0,2); ?>
+
+<!DOCTYPE html>
+<!--[if IE 9 ]><html class="ie9" lang="da"> <![endif]-->
+<!--[if (gt IE 9)|!(IE)]><!--><html lang="da"><!--<![endif]-->
+
+  <head>
+    <title>Business.dk</title>
+    <?php include("./inc/head.php"); ?> 
+  </head>
+  <body>
+
+
+
+  <!--[if lt IE 9]>
+      <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to get the most out of Business.dk.</p>
+  <![endif]-->
+
+
+<div class="container">
+  
+<div class="row">
+<div class="col-md-12 text-center">
+  
+    <div class="spinner">
+          <div class="bounce1"></div>
+          <div class="bounce2"></div>
+          <div class="bounce3"></div>
+    </div>
+
+</div>
+</div>
+</div>
+
+
+  
+
+  
+    
+
+
+
+  
+
+    
+  </body>
+</html>
