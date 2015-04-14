c311698a22158a4184c065f3281d6add2cd23437
Bo Skakke <bo@skakke.dk>
2015-04-14 16:39:56 +0200

    sms button

--- dist/article.php
+++ dist/article.php
@@ -109,7 +109,7 @@
                     <a href="#" class="icon-social-facebook"><i class="fa fa-facebook"></i></a>
                     <a href="#" class="icon-social-twitter"><i class="fa fa-twitter"></i></a>
                     <a href="#" class="icon-social-linkedin"><i class="fa fa-linkedin"></i></a>
-                    <a href="#" class="icon-social-sms visible-inline-block-xs"><i class="fa fa-mobile"></i></a>
+                    <a href="#" class="icon-social-sms visible-xs-inline-block"><i class="fa fa-mobile"></i></a>
 
                     <a href="mailto:?subject=Artikel fra Business.dk&body=[ARTICLE URL]" class="icon-social-mail"><i class="fa fa-envelope"></i></a>
                     <span class="pull-right comments-teaser"><a href="#" class="icon-text scroll-comments"><? dummy("text@number") ?> kommentarer</a> <a href="#" class="icon-social-default scroll-comments"><i class="fa fa-comment"></i></a></span>
