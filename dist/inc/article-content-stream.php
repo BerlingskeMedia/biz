0facde7b16ef1d1427021db589380e61c48250a5
Bo Skakke <bo@skakke.dk>
2015-04-23 12:58:05 +0200

    teasers

--- dist/inc/article-content-stream.php
+++ dist/inc/article-content-stream.php
@@ -82,6 +82,19 @@
         </div>    
         </li>
     <? endwhile ?>
+    <li class="teaser teaser-brandview">
+      <div class="teaser-body">
+      <span class="teaser-tagline">
+    Sponseret af Columbus
+  </span>
+    <h2 class="header"><a href="article-brandview.php"><? dummy("text@headline") ?></a></h2>
+    <footer><a href="section.php" class="cat">Sponseret</a> <time><? dummy("text@time-ago-short") ?></time></footer>
+
+    
+
+  </div>
+
+    </li>
     </ul>
 
 </section>
@@ -154,6 +167,8 @@
         </div>    
         </li>
     <? endwhile ?>
+    
+
     <li class="teaser teaser-brandview">
       <div class="teaser-body">
       <span class="teaser-tagline">
@@ -167,16 +182,6 @@
   </div>
 
     </li>
-
-    <? while (dumb_luck("4")): ?>
-      
-        <li class="teaser">
-            <div class="teaser-body">
-          <h2 class="header"><a href="article.php"><? dummy("text@headline") ?></a></h2>
-          <footer><a href="section.php" class="cat"><? dummy("text@biz-cat") ?></a>  <time> <? dummy("text@time-ago-short") ?></time></footer>
-        </div>    
-        </li>
-    <? endwhile ?>
 
     
 
