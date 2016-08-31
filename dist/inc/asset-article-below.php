332cc8dc3ff1a0552422b44b09808176b4b83314
Bo Skakke <bo@skakke.dk>
2016-08-31 11:03:10 +0200

    remove jobindex#

--- dist/inc/asset-article-below.php
+++ dist/inc/asset-article-below.php
@@ -372,8 +372,7 @@
   
 
   
-  <!-- <iframe src="inc/iframe-jobindex.php" class="iframe-jobindex" frameborder="0" scrolling="auto"></iframe> -->
-  <iframe src="http://www.jobindex.dk/cgi/partnerbanner.cgi?size=300x400&partner=business&pagetype=front;cre=mu;target=_blank;ord=6ca81a82-b751-11e4-b4f6-00259006236a;EASClick=http://ad1.emediate.dk/eas/cu=24881::camp=347212::no=360322::kw=link1-360322::cat1=bond-2944::encoding=utf-8::EASTpage_type=frontpage::EASTpath=%2Fforside::EASTauth=0::c2-2=0::uuid=6ca81a82-b751-11e4-b4f6-00259006236a::EASLink=" height="400" width="300" frameborder="0" scrolling="auto"></iframe>
+  
   
 
 
