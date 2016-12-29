9b61cbc3ca7f1f05fa67a148e4db40cad6d3fb22
Bo Skakke <bo@skakke.dk>
2016-12-29 12:31:10 +0100

    paywall

--- dist/inc/paywall.php
+++ dist/inc/paywall.php
@@ -42,7 +42,6 @@
     </section>
 
 
-
 <?php elseif ($paywall == 'hard'): ?>
 
     <section class=" paywall paywall-hard" rel="noindex">
@@ -55,7 +54,7 @@
             <p>Med et digitalt abonnement får du adgang til alt indhold.</p>
                 
              <div class="module-sm">
-                 <button class="btn btn-live btn-md  btn-paywall btn-paywall-2 ">
+                 <button class="btn btn-live btn-md  btn-paywall btn-paywall-2  ">
                  	Få den første måned gratis nu <i class="fa fa-angle-right"></i>
                  </button>
 			</div>
@@ -67,15 +66,59 @@
                     <i class="fa fa-user" aria-hidden="true"></i>&nbsp;Klik her
                 </a>
             </p>
-            <!-- <div class="alert alert-info">
-                <p>Du er logget ind med en profil, der desværre ikke giver adgang til Business.dk i dag. Det kan skyldes, at dit abonnement har begrænset digital adgang, eller at din ordre ikke er tilknyttet din profil. 
-                <a href="https://b.kundeunivers.dk/">Tilknyt ordrenummer</a></p>
-                <p><strong>Har du brug for hjælp?</strong> Kontakt Kundeservice på 3375 3330</p>
-            </div> -->
-                
-        </div>
     </section>
 
+    <?php elseif ($paywall == 'split'): ?>
 
+        <section class=" paywall paywall-hard" rel="noindex">
+            
+            <div class="paywall-inner">
+            <p class="small">Artiklen er kun for abonnenter</p>
+
+                <h2>Du har ikke adgang til at læse artiklen</h2>
+
+                <p>Du er logget ind med en profil, der ikke giver adgang til at læse artiklen.</p>
+
+                <p>Sådan kommer du videre:</p>
+                    
+                <div class="row">
+                    <div class="col-xs-12 col-md-6">
+                     <a href="http://b.kundeunivers.dk/" class="btn btn-live btn-md  btn-paywall btn-paywall-2 btn-block btn-arrow module-sm">
+                        Jeg har abonnement <i class="fa fa-angle-right"></i><br>
+                        <small>Registrér dit abonnement</small>
+                     </a>   
+                    </div>
+
+                    <div class="col-xs-12 col-md-6">
+                     <a href="http://abonnement.b.dk/berlingske-to-produkter-paywallbanner/" class="btn btn-live btn-md  btn-paywall btn-paywall-2 btn-block btn-arrow module-sm">
+                        Jeg har ikke abonnement <i class="fa fa-angle-right"></i><br>
+                        <small>Køb adgang</small>
+                     </a>   
+                    </div>
+                </div>
+                 <div class="module-sm">
+                     
+                </div>
+                    
+                
+                <p class="small">
+                    Har du brug for hjælp? Kontakt Kundeservice på 3375 3300.
+                </p>
+        </section>
+
+    <?php elseif ($paywall == 'weekend'): ?>
+
+            <section class=" paywall paywall-hard" rel="noindex">
+                
+                <div class="paywall-inner">
+                <p class="small">Artiklen er kun for abonnenter</p>
+
+                    <h2>Dit abonnement giver kun adgang i weekenden.</h2>
+
+                    
+                    <p class="small">
+                        Kontakt Kundeservice på 3375 3300 for at høre, hvordan du får adgang alle ugens syv dage.
+                    </p>
+            </section>
 
 <?php endif; ?>
